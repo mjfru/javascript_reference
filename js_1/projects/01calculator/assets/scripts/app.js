c4eb0ddf03dd03d793f8604ab81ef67c8a30cf9b
Matt Frutchey <mjfru201@gmail.com>
2024-09-10 01:20:24 -0400

    add multiple functions for calculator-like functionality

--- js_1/projects/01calculator/assets/scripts/app.js
+++ js_1/projects/01calculator/assets/scripts/app.js
@@ -1,5 +1,48 @@
-let currentResult = 0;
+const defaultResult = 0;
+let currentResult = defaultResult;
+let logEntries = [];
 
-currentResult = currentResult + 10;
+function getUserNumberInput() {
+	return parseInt(userInput.value);
+}
 
-outputResult(currentResult, '');
+function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
+	const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
+  outputResult(currentResult, calcDescription);
+}
+
+function add() {
+	const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+	// HTML gives us strings, always. Therefore we need to convert it to a number.
+	currentResult += enteredNumber;
+  createAndWriteOutput('+', initialResult, enteredNumber);
+  logEntries.push(enteredNumber);
+}
+
+function subtract() {
+	const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+	currentResult -= enteredNumber;
+	createAndWriteOutput('-', initialResult, enteredNumber);
+}
+
+function multiply() {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+	currentResult *= enteredNumber;
+	createAndWriteOutput('*', initialResult, enteredNumber);
+}
+
+function divide() {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+	currentResult /= enteredNumber;
+	createAndWriteOutput('/', initialResult, enteredNumber);
+}
+
+// Look at this function and execute it for me when a click occurs
+addBtn.addEventListener("click", add);
+subtractBtn.addEventListener("click", subtract);
+multiplyBtn.addEventListener("click", multiply);
+divideBtn.addEventListener("click", divide);
