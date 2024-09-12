9def2b85218e25a17058c2429094e20de6d794a2
Matt Frutchey <mjfru201@gmail.com>
2024-09-12 09:45:51 -0400

    finalize functions for unconventiona calculator

--- js_1/projects/01calculator/assets/scripts/app.js
+++ js_1/projects/01calculator/assets/scripts/app.js
@@ -8,37 +8,56 @@ function getUserNumberInput() {
 
 function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
 	const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
-  outputResult(currentResult, calcDescription);
+	outputResult(currentResult, calcDescription);
+}
+
+function writeToLog(
+	operationIdentifier,
+	prevResult,
+	operationNumber,
+	newResult
+) {
+	const logEntry = {
+		operation: operationIdentifier,
+		prevResult: prevResult,
+		number: operationNumber,
+		result: newResult,
+	};
+	logEntries.push(logEntry);
+	console.log(logEntries);
 }
 
 function add() {
 	const enteredNumber = getUserNumberInput();
-  const initialResult = currentResult;
+	const initialResult = currentResult;
 	// HTML gives us strings, always. Therefore we need to convert it to a number.
 	currentResult += enteredNumber;
-  createAndWriteOutput('+', initialResult, enteredNumber);
-  logEntries.push(enteredNumber);
+	createAndWriteOutput("+", initialResult, enteredNumber);
+	writeToLog("ADD", prevResult, operationNumber, currentResult);
 }
 
 function subtract() {
 	const enteredNumber = getUserNumberInput();
-  const initialResult = currentResult;
+	const initialResult = currentResult;
 	currentResult -= enteredNumber;
-	createAndWriteOutput('-', initialResult, enteredNumber);
+	createAndWriteOutput("-", initialResult, enteredNumber);
+	writeToLog("SUBTRACT", prevResult, operationNumber, currentResult);
 }
 
 function multiply() {
-  const enteredNumber = getUserNumberInput();
-  const initialResult = currentResult;
+	const enteredNumber = getUserNumberInput();
+	const initialResult = currentResult;
 	currentResult *= enteredNumber;
-	createAndWriteOutput('*', initialResult, enteredNumber);
+	createAndWriteOutput("*", initialResult, enteredNumber);
+  writeToLog("MULTIPLY", prevResult, operationNumber, currentResult);
 }
 
 function divide() {
-  const enteredNumber = getUserNumberInput();
-  const initialResult = currentResult;
+	const enteredNumber = getUserNumberInput();
+	const initialResult = currentResult;
 	currentResult /= enteredNumber;
-	createAndWriteOutput('/', initialResult, enteredNumber);
+	createAndWriteOutput("/", initialResult, enteredNumber);
+  writeToLog("DIVIDE", prevResult, operationNumber, currentResult);
 }
 
 // Look at this function and execute it for me when a click occurs
