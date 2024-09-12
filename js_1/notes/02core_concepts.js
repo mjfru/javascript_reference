3d90fee9cad2250ee900e594edfc464b9f51e5b4
Matt Frutchey <mjfru201@gmail.com>
2024-09-12 09:44:22 -0400

    add more overviews of data types and the defer keyword

--- js_1/notes/02core_concepts.js
+++ js_1/notes/02core_concepts.js
@@ -33,6 +33,28 @@ let [keyword] = [value]
   - Wrapped in quotes (single or double, must match) or back-ticks ``
   - Important for outputting results
 
+* Booleans (True / False)
+  - true / false, and only true or false
+  - Important for conditional code and situations where there are only two options.
+
+* Objects
+  - { name: "Matt", age: 31 }
+  - Key value pairs that are important for grouped / related data; helps you with organizing data.
+
+* Arrays
+  - [1, 3, "hello"]
+  - A list of data, useful for unknown amounts of data
+  - Allowed to mix data types in arrays
+
+* Null / Undefined / NaN (Not a Number)
+  Undefined - Default value of uninitialized variables, indicates nothing is there!
+    - Should never be assigned as a value manually.
+  Null - No data but is never assumed by default.
+    - It's set manually if you want to 'reset/clear' a variable.
+  NaN - Technically not a type but it's a type of -number- and can be used in calculations.
+    - It yields a new NaN and it's the result of invalid calculations. (3 * 'hi', for example)
+
+
 ! Functions
 * Code 'on demand'
   1. Define Function
@@ -42,6 +64,11 @@ let [keyword] = [value]
 * To execute a function, you call it.
   2. greet('Matt');
 * And it can be called as often as you want with different parameters.
+
+! The 'defer' Keyword
+* Adding 'defer' inside the opening tag of <script> allows the browser to download the script but only executes it once HTML and other elements have been parsed.
+* Adding 'async' does not wait for HTML to be parsed and executes as soon as possible. Better for scripts that have nothing to do with appearance of the webpage and doesn't rely on HTML content.
+? These are only for external scripts, not any written in the HTML itself.
 */
 
 let userName = "Matt"; // Matt
