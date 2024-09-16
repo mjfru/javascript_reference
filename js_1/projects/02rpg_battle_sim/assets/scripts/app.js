8996b52a9f33dcbcff2f706049ad123e77a5f7d0
Matt Frutchey <mjfru201@gmail.com>
2024-09-16 07:17:55 -0400

    condense logic, add endRound, incorporate bonus life, etc. with conditional checks

--- js_1/projects/02rpg_battle_sim/assets/scripts/app.js
+++ js_1/projects/02rpg_battle_sim/assets/scripts/app.js
@@ -1,25 +1,87 @@
 const ATTACK_VALUE = 10;
+const STRONG_ATTACK_VALUE = 17;
 const MONSTER_ATTACK_VALUE = 14;
+const HEAL_VALUE = 20;
 
 let chosenMaxLife = 100;
 let currentMonsterHealth = chosenMaxLife;
 let currentPlayerHealth = chosenMaxLife;
+let hasBonusLife = true;
 
 adjustHealthBars(chosenMaxLife);
 
-function attackHandler() {
-	const damage = dealMonsterDamage(ATTACK_VALUE);
-	currentMonsterHealth -= damage;
+function reset() {
+	currentMonsterHealth = chosenMaxLife;
+	currentPlayerHealth = chosenMaxLife;
+	resetGame(chosenMaxLife);
+}
+
+//* Ensuring the enemy hits the player after every turn, regardless of player's action.
+//* Also checking win conditions after each turn.
+function endRound() {
+	const initialPlayerHealth = currentPlayerHealth;
 	const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
 	currentPlayerHealth -= playerDamage;
 
+	if (currentPlayerHealth <= 0 && hasBonusLife) {
+		hasBonusLife = false;
+		removeBonusLife();
+		currentPlayerHealth = initialPlayerHealth;
+		setPlayerHealth(initialPlayerHealth);
+		alert("Bonus life to the rescue!");
+	}
+
 	if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
 		alert("Winner winner, chicken dinner!");
+
 	} else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
 		alert("Whoops, time to grind some more. Play again?");
+
 	} else if (currentPlayerHealth <= 0 && currentMonsterHealth <= 0) {
 		alert("Draw!");
+
 	}
+
+  if (currentMonsterHealth <= 0 || currentPlayerHealth <= 0) {
+    reset();
+  }
+}
+
+function attackMonster(attackType) {
+	let maxDamage;
+	if (attackType === "ATTACK") {
+		maxDamage = ATTACK_VALUE;
+	} else {
+		maxDamage = STRONG_ATTACK_VALUE;
+	}
+
+	const damage = dealMonsterDamage(maxDamage);
+	currentMonsterHealth -= damage;
+	endRound();
+}
+
+function attackHandler() {
+	attackMonster("ATTACK");
+}
+
+function strongAttackHandler() {
+	attackMonster("STRONG_ATTACK");
+}
+
+function healPlayerHandler() {
+	let healValue;
+	if (currentPlayerHealth >= chosenMaxLife - HEAL_VALUE) {
+		alert("You can't heal more than your max HP.");
+		//* Heal up to max health, but not above it.
+		healValue = chosenMaxLife - currentPlayerHealth;
+	} else {
+		healValue = HEAL_VALUE;
+	}
+	increasePlayerHealth(HEAL_VALUE);
+	currentPlayerHealth += healValue;
+	endRound();
 }
 
 attackBtn.addEventListener("click", attackHandler);
+strongAttackBtn.addEventListener("click", strongAttackHandler);
+healBtn.addEventListener("click", healPlayerHandler);
