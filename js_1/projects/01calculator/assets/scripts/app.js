7bb165fa2ee082ba6643b6bdf248da1f9b58fe9e
Matt Frutchey <mjfru201@gmail.com>
2024-09-25 01:15:01 -0400

    implement .bind() into calc functions, refactor

--- js_1/projects/01calculator/assets/scripts/app.js
+++ js_1/projects/01calculator/assets/scripts/app.js
@@ -27,41 +27,62 @@ function writeToLog(
 	console.log(logEntries);
 }
 
-function add() {
-	const enteredNumber = getUserNumberInput();
-	const initialResult = currentResult;
-	// HTML gives us strings, always. Therefore we need to convert it to a number.
-	currentResult += enteredNumber;
-	createAndWriteOutput("+", initialResult, enteredNumber);
-	writeToLog("ADD", prevResult, operationNumber, currentResult);
-}
+// function add() {
+// 	const enteredNumber = getUserNumberInput();
+// 	const initialResult = currentResult;
+// 	// HTML gives us strings, always. Therefore we need to convert it to a number.
+// 	currentResult += enteredNumber;
+// 	createAndWriteOutput("+", initialResult, enteredNumber);
+// 	writeToLog("ADD", prevResult, operationNumber, currentResult);
+// }
 
-function subtract() {
-	const enteredNumber = getUserNumberInput();
-	const initialResult = currentResult;
-	currentResult -= enteredNumber;
-	createAndWriteOutput("-", initialResult, enteredNumber);
-	writeToLog("SUBTRACT", prevResult, operationNumber, currentResult);
-}
+// function subtract() {
+// 	const enteredNumber = getUserNumberInput();
+// 	const initialResult = currentResult;
+// 	currentResult -= enteredNumber;
+// 	createAndWriteOutput("-", initialResult, enteredNumber);
+// 	writeToLog("SUBTRACT", prevResult, operationNumber, currentResult);
+// }
 
-function multiply() {
-	const enteredNumber = getUserNumberInput();
-	const initialResult = currentResult;
-	currentResult *= enteredNumber;
-	createAndWriteOutput("*", initialResult, enteredNumber);
-  writeToLog("MULTIPLY", prevResult, operationNumber, currentResult);
-}
+// function multiply() {
+// 	const enteredNumber = getUserNumberInput();
+// 	const initialResult = currentResult;
+// 	currentResult *= enteredNumber;
+// 	createAndWriteOutput("*", initialResult, enteredNumber);
+// 	writeToLog("MULTIPLY", prevResult, operationNumber, currentResult);
+// }
 
-function divide() {
+// function divide() {
+// 	const enteredNumber = getUserNumberInput();
+// 	const initialResult = currentResult;
+// 	currentResult /= enteredNumber;
+// 	createAndWriteOutput("/", initialResult, enteredNumber);
+// 	writeToLog("DIVIDE", prevResult, operationNumber, currentResult);
+// }
+
+function calculate(operation) {
 	const enteredNumber = getUserNumberInput();
 	const initialResult = currentResult;
-	currentResult /= enteredNumber;
-	createAndWriteOutput("/", initialResult, enteredNumber);
-  writeToLog("DIVIDE", prevResult, operationNumber, currentResult);
+	let operator;
+	if (operation === "ADD") {
+		currentResult += enteredNumber;
+		operator = "+";
+	} else if (operation === "SUBTRACT") {
+		currentResult -= enteredNumber;
+		operator = "-";
+	} else if (operation === "MULTIPLY") {
+		currentResult *= enteredNumber;
+		operator = "*";
+	} else {
+		currentResult /= enteredNumber;
+		operator = "/";
+	}
+	createAndWriteOutput(operator, initialResult, enteredNumber);
+	writeToLog(operation, initialResult, enteredNumber, currentResult);
 }
 
 // Look at this function and execute it for me when a click occurs
-addBtn.addEventListener("click", add);
-subtractBtn.addEventListener("click", subtract);
-multiplyBtn.addEventListener("click", multiply);
-divideBtn.addEventListener("click", divide);
+addBtn.addEventListener("click", calculate.bind(this, "ADD"));
+subtractBtn.addEventListener("click", calculate.bind(this, "SUBTRACT"));
+multiplyBtn.addEventListener("click", calculate.bind(this, "MULTIPLY"));
+divideBtn.addEventListener("click", calculate.bind(this, "DIVIDE"));
