28cd0b6382e7bd230d51ebace0fa7e6471732aac
Matt Frutchey <mjfru201@gmail.com>
2024-10-03 11:43:00 -0400

    begin styling dom elements review notes

---
+++ js_1/notes/07styling_dom_elements.js
@@ -0,0 +1,18 @@
+/*
+! Styling DOM Elements
+
+* Via style property:
+  Directly targets individual CSS styles (on the element)
+  Controls styles as inline styles on the element
+  Style property names are based on CSS properties but have adjusted, camelCase names
+
+* Via className
+  Directly set the CSS classes assigned to the element
+  Set / Control all classes at once
+  You can also control the id or other properties
+
+* Via classList
+  Conveniently add, remove, or toggle CSS classes
+  Fine-grained control over classes that are added
+  Can be used with className (with care)
+*/
