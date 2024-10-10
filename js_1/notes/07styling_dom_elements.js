369b37b9510e1b264c86ce3de2db36c63402f8b4
Matt Frutchey <mjfru201@gmail.com>
2024-10-10 13:14:54 -0400

    add notes for creating and inserting elements

--- js_1/notes/07styling_dom_elements.js
+++ js_1/notes/07styling_dom_elements.js
@@ -15,4 +15,46 @@
   Conveniently add, remove, or toggle CSS classes
   Fine-grained control over classes that are added
   Can be used with className (with care)
+
+! Creating & Inserting Elements
+
+* HTML String
+  innerHTML
+    - The allows us to add HTML directly, tags and all!
+      example.innerHTML = '<h2>Example Text</h2>'
+  add (render) HTML string
+  insertAdjacentHTML()
+    - Allows us to target a position and choose what we want to insert.
+      example.insertAdjacementHTML('beforeend', '<p>Some Text</p>')
+  Add (render) HTML string in specific position
+
+* createElement()
+  ! For use only on document
+  document.createElement('tagName');
+    const newLi = document.createElement('li');
+    example.appendChild(newLi)
+    newLi.textContet = 'Item #';
+
+  appendChild / append()
+  * append() is a little different, it just adds a text node with your chosen text inside.
+  
+  prepend(), before(), after(), afterend(), insertBefore()
+  * These allow us greater flexibility to where we can insert an element.
+    list.lastElementChild.before(newLi)
+    list.lastElementChild.after(newLi)
+
+  replaceChild(), replaceWith()
+  Replace existing DOM element / node with a new one
+
+! Cloning DOM Nodes
+* Copying an element is possible with cloneNode() taking one argument (optional), a boolean, specifying if children elements should also be cloned.
+  const newLi2 = newLi.cloneNode(true)
+  list.append(newLi, newLi2)
+
+! Removing Elements
+* To simply get rid of an element:
+const list = document.querySelector('ul')
+list.remove();
+OR
+list.parentElement.removeChild(list) -- If above is not available due to browser restrictions
 */
