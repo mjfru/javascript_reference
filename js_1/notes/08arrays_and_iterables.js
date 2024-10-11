2f551ba99d3a527cf3d44c472e7b4a7d7e782d0d
Matt Frutchey <mjfru201@gmail.com>
2024-10-11 09:59:41 -0400

    begin review notes on arrays and iterables

---
+++ js_1/notes/08arrays_and_iterables.js
@@ -0,0 +1,18 @@
+/*
+! Arrays & Other Iterables in JavaScript
+
+? What's an iterable?
+* - Objects that implement the 'iterable' protocol and have an @@iterator method.
+
+? In human terms...
+* - Objects where you can use the for-of loop
+
+! Not every iterable is an array! Other iterables include: NodeLists, Strings, Maps, and Sets.
+
+? What's an Array-like Object?
+* - Technically, these have a length property and use indexes to access items.
+
+! Not every array-like object is an array! Other array-likes are NodeLists and Strings.
+
+
+*/
