747392e03dee7b335cb2f35ec9c22e489c30e37b
Matt Frutchey <mjfru201@gmail.com>
2024-11-03 13:00:51 -0500

    add splice, slice, and find examples

--- js_1/exercises/08arrays/01creating_and_changing_arrays/app.js
+++ js_1/exercises/08arrays/01creating_and_changing_arrays/app.js
@@ -1,3 +1,5 @@
+//! Creating, Add, & Removing from Arrays
+
 //! Most common way to create an array
 const numbers = [1, 2, 3];
 
@@ -41,4 +43,45 @@ hobbies.shift();
 //* Using an index directly to add or remove items
 hobbies[1] = "Traveling";
 hobbies[5] = "Hiking";
+// console.log(hobbies);
+
+//! Splice
+//* .splice() allows us to insert items in between others in an array.
+// It typically takes two arguments, a starting index and the amount of indexes you want to delete after that, and what you want to insert from the starting position.
+//* Inserting something with .splice():
+hobbies.splice(1, 0, "Eating good food");
+// console.log(hobbies);
+
+//* Deleting something with .splice():
+hobbies.splice(0, 1);
 console.log(hobbies);
+
+//! Slice
+//* .slice() copies an array and creates a brand new one
+const testResults = [1, 5.3, 1.5, 10.99, -5, 10];
+const savedResults = testResults.slice();
+console.log(savedResults);
+
+//* Using .slice() to select only a part of an array:
+//? .slice(startIndex, endIndex (optional))
+const selectedResults = testResults.slice(0, 2);
+console.log(selectedResults);
+
+const allButFirst = testResults.slice(1);
+console.log(allButFirst);
+
+//! Adding & Returning a new array with .concat()
+const newResults = testResults.concat([3, 64, 11]);
+console.log(newResults);
+
+//! Retrieving the Index of an Item
+//* .indexOf, without a second argument, searches the entire array for the FIRST matched value.
+console.log(testResults.indexOf(5.3));
+//* This is the same, but starts at the right of the array
+console.log(testResults.lastIndexOf(5.3));
+
+const personData = [{ name: "Matt" }, { name: "Max" }];
+console.log(personData.indexOf({ name: "Matt" }));
+//! Objects are never identical, so this won't work.
+
+//! Here, we need .find() or .findIndex()
