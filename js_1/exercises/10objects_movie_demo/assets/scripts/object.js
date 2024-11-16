9e2d07de9a7b3ec9127870c660b05d3241d14ab4
Matt Frutchey <mjfru201@gmail.com>
2024-11-16 14:32:29 -0500

    add example context for .filter(), .bind(), etc., and use cases for 'this'

--- js_1/exercises/10objects_movie_demo/assets/scripts/object.js
+++ js_1/exercises/10objects_movie_demo/assets/scripts/object.js
@@ -3,6 +3,47 @@ const searchBtn = document.getElementById("search-btn");
 
 const movies = [];
 
+const renderMovies = (filter = "") => {
+	const movieList = document.getElementById("movie-list");
+
+	if (movies.length === 0) {
+		movieList.classList.remove("visible");
+	} else {
+		movieList.classList.add("visible");
+	}
+	movieList.innerHTML = "";
+
+	const filteredMovies = !filter
+		? movies
+		: movies.filter((movie) => movie.info.title.includes(filter));
+
+	filteredMovies.forEach((movie) => {
+		const movieElement = document.createElement("li");
+		// if (!(movie.info === undefined)) {
+		// }
+
+		//? Destructuring for readability:
+		const { info, ...otherProps } = movie;
+		console.log(otherProps); // the id
+		// const { title } = info;
+
+    const { getFormattedTitle } = movie;
+    //? When this function executes, bind() guarantees 'this' will refer to the movie object
+    // getFormattedTitle = getFormattedTitle.bind(movie)
+
+    //? Call is different from .bind(); .call() executes the function right away.
+    //? .apply() takes only one additional argument, an array
+		let text = getFormattedTitle.call(movie) + " - ";
+		for (const key in info) {
+			if (key !== "title") {
+				text = text + `${key}: ${info[key]}`;
+			}
+		}
+		movieElement.textContent = text;
+		movieList.append(movieElement);
+	});
+};
+
 const addMovieHandler = () => {
 	const title = document.getElementById("title").value;
 	const extraName = document.getElementById("extra-name").value;
@@ -21,11 +62,23 @@ const addMovieHandler = () => {
 			title,
 			[extraName]: extraValue,
 		},
-		id: Math.random(),
+		id: Math.random().toString(),
+		getFormattedTitle() {
+      console.log(this);
+			// Refers to whatever calls this function in your code
+			// Without the 'this', it would look outside the function's scope for a variable or constant called 'info'.
+			return this.info.title.toUpperCase();
+		},
 	};
 
-  movies.push(newMovie);
-  console.log(newMovie);
+	movies.push(newMovie);
+	renderMovies();
 };
 
-addMovieBtn.addEventListener('click', addMovieHandler);
+const searchMovieHandler = () => {
+	const filteredTerm = document.getElementById("filter-title").value;
+	renderMovies(filteredTerm);
+};
+
+addMovieBtn.addEventListener("click", addMovieHandler);
+searchBtn.addEventListener("click", searchMovieHandler);
