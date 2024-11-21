2bffb9c029e591504e1097c57ea0c1510190571a
Matt Frutchey <mjfru201@gmail.com>
2024-11-20 19:05:33 -0500

    begin oop review notes

---
+++ js_1/notes/13oop.js
@@ -0,0 +1,17 @@
+/*
+! Object-Oriented Programming (OOP) in JavaScript
+* OOP is an idea that you work with (real life) entities in your code.
+* It allows us to think about our code based on real life connections.
+
+ProductList
+  - Renders a list of products fetched from a server
+  - Holds the rendering and fetching logic
+
+Product
+  - Renders details about a product and allows addition to a cart
+  - Holds rendering and cart-adding logic
+
+ShoppingCart
+  - Renders cart products and total, allows user to order
+  - Holds rendering and ordering (server comms) logic.
+*/
