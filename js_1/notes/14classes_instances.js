78d22d53cd7b5b7b0dec5ad7f6dc97413ffd1e3a
Matt Frutchey <mjfru201@gmail.com>
2024-12-02 12:22:15 -0500

    add notes on static properties and inheritance

--- js_1/notes/14classes_instances.js
+++ js_1/notes/14classes_instances.js
@@ -26,4 +26,32 @@ class Product {
   }
 
 }
+
+! Static Properties, Fields, & Methods
+* These properties, fields, and methods are defined with the keyword 'static'.
+* They are only accessible on the class itself, not on an instance of the class!
+Typically, these are used in helper classes, global configuration, etc.
+
+? Instance fields, properties, and methods
+* These are defined -without- any keyword, are only accessible on the instances of the class, and are used for core, re-usable logic
+
+! When to use Classes?
+* Object literals are great for general data grouping or objects that you only create once; they are quick and easy with no overhead.
+
+* Classes, however, are great when you re-create the same type of object over and over again
+* More overhead initially but easy 'object duplication' thereafter.
+
+! Inheritance
+* A base class holds some properties or methods and can be extended to other classes.
+
+? Base Class:
+Post { title, text, creatorId }
+
+? New Class:
+ImagePost { title, text, creatorId, imageURL, imageDesc }
+
+* Building multiple classes with all this duplication would be a pain.
+! You can therefore extend the base class to the individual ones to avoid repeating yourself and creating DRY code.
+
+
 */
