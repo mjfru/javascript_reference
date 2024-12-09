1ff48b8b6daca6c7f4dc8bcfac3c8e9d4a38ed20
Matt Frutchey <mjfru201@gmail.com>
2024-12-09 10:46:31 -0500

    initialize project for oop practice

---
+++ js_1/projects/05oop_project_planner/assets/scripts/app.js
@@ -0,0 +1,19 @@
+class Tooltip {}
+
+class ProjectItem {}
+
+class ProjectList {
+	constructor(type) {
+		const projectItems = document.querySelectorAll(`#${type}-projects li`);
+    console.log(projectItems);
+	}
+}
+
+class App {
+	static init() {
+		const activeProjectsList = new ProjectList("active");
+		const finishedProjectsList = new ProjectList("finished");
+	}
+}
+
+App.init();
