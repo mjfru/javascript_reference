fde692c662d8925f2e413c4be2e6e563e0fbb364
Matt Frutchey <mjfru201@gmail.com>
2024-12-23 12:53:56 -0500

    begin adv function notes with pure / impure functions

---
+++ js_1/notes/17adv_function_concepts.js
@@ -0,0 +1,5 @@
+/*
+! Pure Functions & Side Effects
+* A Pure Function is a function that, with a given input, always provides the SAME output and does not trigger any side effects.
+? Triggering a side effect means it does not change anything outside of the function itself (setting values, etc.)
+*/
