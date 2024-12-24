60ebea445c53b887a19e002a8cb70cb6107398f6
Matt Frutchey <mjfru201@gmail.com>
2024-12-24 00:12:16 -0500

    add more notes on pure functions and best practices

--- js_1/notes/17adv_function_concepts.js
+++ js_1/notes/17adv_function_concepts.js
@@ -2,4 +2,8 @@
 ! Pure Functions & Side Effects
 * A Pure Function is a function that, with a given input, always provides the SAME output and does not trigger any side effects.
 ? Triggering a side effect means it does not change anything outside of the function itself (setting values, etc.)
+
+It's typically a good aim to try to program pure functions when possible.
+It eliminates guess work and is easy to understand but it's often impossible to rely only on pure functions.
+Rather, we should try out best to code pure first and impure only when it's essential.
 */
