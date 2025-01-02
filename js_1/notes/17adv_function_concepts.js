1e75a791d63db899af3873080dfa8c206f754fd6
Matt Frutchey <mjfru201@gmail.com>
2025-01-02 10:04:12 -0500

    add notes and example about factory functions

--- js_1/notes/17adv_function_concepts.js
+++ js_1/notes/17adv_function_concepts.js
@@ -6,4 +6,24 @@
 It's typically a good aim to try to program pure functions when possible.
 It eliminates guess work and is easy to understand but it's often impossible to rely only on pure functions.
 Rather, we should try out best to code pure first and impure only when it's essential.
+
+! Factory Functions - A function that produces another function
+If you have a function you call multiple times throughout your app and it can be pre-configured, we can use these to make our code DRY-er.
 */
+
+function createTaxCalculator(tax) {
+	function calculateTax(amount) {
+		return amount * tax;
+	}
+	return calculateTax;
+}
+
+// What's the benefit? We don't need the tax percentage as an input and can thus do this:
+
+const calculateVatAmount = createTaxCalculator(0.19);
+const calculateIncomeTaxAmount = createTaxCalculator(0.25);
+
+console.log(calculateVatAmount(100));
+console.log(calculateVatAmount(200));
+
+
