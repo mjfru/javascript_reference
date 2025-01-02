d617b80adbb27ea229a03d461d294c018329fa70
Matt Frutchey <mjfru201@gmail.com>
2025-01-02 10:40:51 -0500

    add annotated closure exercise

--- js_1/exercises/16adv_functions/app.js
+++ js_1/exercises/16adv_functions/app.js
@@ -1,7 +1,6 @@
-
 //! Pure
 function add(num1, num2) {
-  return num1 + num2;
+	return num1 + num2;
 }
 
 console.log(add(1, 5));
@@ -9,7 +8,7 @@ console.log(add(12, 15));
 
 //! Impure
 function addRandom(num1) {
-  return num1 + Math.random()
+	return num1 + Math.random();
 }
 
 console.log(addRandom(5));
@@ -18,16 +17,31 @@ console.log(addRandom(5));
 let previousResult = 0;
 
 function addMore(num1, num2) {
-  const sum = num1 + num2;
-  previousResult = sum;
-  return sum;
+	const sum = num1 + num2;
+	previousResult = sum;
+	return sum;
 }
 
 const hobbies = ["Coding", "Sports", "Gaming"];
 
 function printHobbies(array) {
-  array.push('New Hobby');
-  console.log(array);
+	array.push("New Hobby");
+	console.log(array);
 }
 
 printHobbies(hobbies);
+
+//! Closure practice
+let username = "James"; // Example #1 - Results in 'Hi James'
+
+function greetUser() {
+	// let name = username; // Example #3 - Results in 'Hi Matt' again
+	let name = "Anna"; // Example #4 - Results in 'Hi Anna' because the inner environemnt wins over the outer.
+  // If it didn't find name in the function, then it would check outside.
+	console.log("Hi " + name); // Changed username to name
+}
+
+let name = "Jim"; // Example #4 - Results in 'Hi Anna'
+username = "Matt"; // Example #2 - Results in 'Hi Matt' now
+
+greetUser();
