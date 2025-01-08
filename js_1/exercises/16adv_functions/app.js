3f4114a8df82119fad0d0785d9a309e2b201d1f1
Matt Frutchey <mjfru201@gmail.com>
2025-01-08 09:21:41 -0500

    add working recursion example, showing benefits with and without it

--- js_1/exercises/16adv_functions/app.js
+++ js_1/exercises/16adv_functions/app.js
@@ -31,13 +31,13 @@ function printHobbies(array) {
 
 printHobbies(hobbies);
 
-//! Closure practice
+//! Closure Practice
 let username = "James"; // Example #1 - Results in 'Hi James'
 
 function greetUser() {
 	// let name = username; // Example #3 - Results in 'Hi Matt' again
 	let name = "Anna"; // Example #4 - Results in 'Hi Anna' because the inner environemnt wins over the outer.
-  // If it didn't find name in the function, then it would check outside.
+	// If it didn't find name in the function, then it would check outside.
 	console.log("Hi " + name); // Changed username to name
 }
 
@@ -45,3 +45,70 @@ let name = "Jim"; // Example #4 - Results in 'Hi Anna'
 username = "Matt"; // Example #2 - Results in 'Hi Matt' now
 
 greetUser();
+
+//! Recursion Practice & Example
+function powerOf(value, power) {
+	let result = 1;
+
+	for (let i = 0; i < power; i++) {
+		result *= value;
+	}
+
+	return result;
+}
+
+console.log(powerOf(2, 3)); // 2 * 2 * 2 = 8
+
+//? Recusion allows us to rewrite the above function in a 'simpler' way, allowing the function to call itself.
+
+function powerOfRecursion(value, power) {
+	if (power === 1) {
+		return value;
+	}
+	return value * powerOfRecursion(value, power - 1);
+}
+
+console.log(powerOfRecursion(2, 3)); // 8
+
+//! Advanced Example:
+const myself = {
+	name: "Matt",
+	friends: [
+		{
+			name: "Jim",
+			friends: [
+				{
+					name: "Scott",
+				},
+			],
+		},
+		{
+			name: "Austin",
+			friends: [
+				{
+					name: "Connor",
+				},
+				{
+					name: "Paige",
+				},
+			],
+		},
+	],
+};
+
+function getFriendNames(person) {
+	const collectedNames = [];
+
+	if (!person.friends) {
+		return [];
+	}
+
+	for (const friend of person.friends) {
+		collectedNames.push(friend.name);
+		collectedNames.push(...getFriendNames(friend));
+	}
+
+	return collectedNames;
+}
+
+console.log(getFriendNames(myself));
