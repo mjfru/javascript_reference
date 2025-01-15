fad6cf57918e2ac2f6e85041fde981f9a1c20e19
Matt Frutchey <mjfru201@gmail.com>
2025-01-15 09:55:50 -0500

    finish promise chaining demo and error handling

--- js_1/exercises/18promises/index.js
+++ js_1/exercises/18promises/index.js
@@ -1,6 +1,21 @@
 const button = document.querySelector("button");
 const output = document.querySelector("p");
 
+const getPosition = (options) => {
+	const promise = new Promise((resolve, reject) => {
+		navigator.geolocation.getCurrentPosition(
+			(success) => {
+				resolve(success);
+			},
+			(error) => {
+				reject(error);
+			},
+			options
+		);
+	});
+	return promise;
+};
+
 const setTimer = (duration) => {
 	const promise = new Promise((resolve, reject) => {
 		setTimeout(() => {
@@ -11,17 +26,20 @@ const setTimer = (duration) => {
 };
 
 function trackUserHandler() {
+	let positionData;
 	console.log("Clicked!");
-	navigator.geolocation.getCurrentPosition(
-		(positionData) => {
-			setTimer(2000).then((data) => {
-				console.log(data, positionData);
-			});
-		},
-		(error) => {
+	getPosition()
+		.then((posData) => {
+			positionData = posData;
+			return setTimer(2000);
+		})
+		.catch((error) => {
 			console.log(error);
-		}
-	);
+			return "We continue nonetheless!";
+		})
+		.then((data) => {
+			console.log(data, positionData);
+		});
 	setTimer(1000).then(() => {
 		console.log("Timer done!");
 	});
