0fcd581989a8188b0c4877a79e70d3e0d5524c86
Matt Frutchey <mjfru201@gmail.com>
2025-01-19 12:17:32 -0500

    add post request example

--- js_1/exercises/20http_requests/assets/scripts/app.js
+++ js_1/exercises/20http_requests/assets/scripts/app.js
@@ -1,7 +1,9 @@
 const listElement = document.querySelector(".posts");
 const postTemplate = document.getElementById("single-post");
+const form = document.querySelector('#new-post form');
+const fetchButton = document.querySelector("#available-posts button")
 
-function sendHttpRequest(method, url) {
+function sendHttpRequest(method, url, data) {
 	const promise = new Promise((resolve, reject) => {
 		const xhr = new XMLHttpRequest();
 
@@ -15,7 +17,7 @@ function sendHttpRequest(method, url) {
 			// const listOfPosts = JSON.parse(xhr.response);
 		};
 
-		xhr.send();
+		xhr.send(JSON.stringify(data));
 	});
 	return promise;
 }
@@ -37,4 +39,24 @@ async function fetchPosts() {
 	}
 }
 
-fetchPosts();
+//! POST
+async function createPost(title, content) {
+	const userId = Math.random();
+	const post = {
+		title: title,
+		body: content,
+		userId: userId,
+	};
+
+	sendHttpRequest("POST", "https://jsonplaceholder.typicode.com/posts", post);
+}
+fetchButton.addEventListener('click', fetchPosts)
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  const enteredTitle = event.currentTarget.querySelector('#title').value;
+  const enteredContent = event.currentTarget.querySelector("#content").value;
+  createPost(enteredTitle, enteredContent);
+})
+
+// fetchPosts();
+// createPost("Dummy post", "Testing...");
