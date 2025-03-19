57bed9c908f26e5d9198840be67411de940be46b
Matt Frutchey <mjfru201@gmail.com>
2025-03-19 12:27:42 -0400

    begin testing notes

---
+++ js_1/notes/31testing.js
@@ -0,0 +1,34 @@
+/*
+! Testing
+Yes, we've tested code before but this is more about automating the process and simplifying it.
+
+? Why Test (Like this)?
+  - Get an error if you break your code
+  - Save time
+  - Think about possible issues & bugs
+  - Integrate into build workflow
+  - Break up complex dependencies
+  - Improves your code
+
+? What types of tests can we use?
+  * - Fully Isolated (testing one function) -> Unit Tests
+      - These are the 'simplest' and thousands will be written.
+  * - With dependencies (testing a function that calls another) -> Integration Tests
+      - These are harder and quite a few of these will be written.
+  * - Full Flow (validating the DOM after a click) -> End-to-End (E2E) Tests
+      - These are the most difficult and rarest.
+
+! Testing Setup
+* Test Runners
+  - Execute your tests, summarize results
+  - E.G. Mocha
+
+* Assertion Libraries
+  - Define testing logic, conditions
+  - E.G. Chai
+! Jest does both!
+
+* Headless Browsers
+  - Simulates browser interaction
+  - E.G. Puppeteer (good for E2E testing)
+*/
