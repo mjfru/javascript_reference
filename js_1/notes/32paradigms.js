3d2fd7dfb386c3fa1f5c3c38d2f0c381db39fce2
Matt Frutchey <mjfru201@gmail.com>
2025-03-22 15:11:38 -0400

    add run down / review of programming paradigms

---
+++ js_1/notes/32paradigms.js
@@ -0,0 +1,19 @@
+/*
+! Programming Paradigms
+* Essentially, this is how you write and organize your code!
+
+There are 3 main paradigms:
+
+? Object-oriented
+  * Organizing data and logic in objects (properties + methods)
+  * Organize code in logical entities, think about real-world terms (products, shopping carts, etc.)
+
+? Procedural
+  * Write a sequential series of execution steps / tasks
+  * "Top to bottom" code execution
+
+? Functional
+  * Organize code in (pure) functions with clearly defined tasks
+  * Pass data around via parameters
+*/
+
